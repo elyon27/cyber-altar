c9a52337571df32ba33a12aa838c66b649d46888
elyon27 <avelar2231@gmail.com>
2026-03-15 00:16:29 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,6 @@ import "./globals.css"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 
-const geistSans = GeistSans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
-
 export default function RootLayout({
   children,
 }: {
@@ -19,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
     </html>
